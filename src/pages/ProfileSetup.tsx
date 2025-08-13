00f8d780e84e0b65420334ee47190625692b34f1
Shalom <kamokoueshalom@gmail.com>
2025-08-13 21:49:18 +0200

    Added /

--- src/pages/ProfileSetup.tsx
+++ src/pages/ProfileSetup.tsx
@@ -250,7 +250,7 @@ const ProfileSetup = () => {
         <CardHeader>
           <CardTitle>Complétez votre profil</CardTitle>
         </CardHeader>
-        <form onSubmit={handleSubmit} noValidate>
+        <form onSubmit={handleSubmit}>
           <CardContent className="space-y-4">
             {step === 1 && (
               <React.Fragment>
